7ae53c07db8098dfec1c1febb21566585f783f51
Vedant <vedantbhamare11@gmail.com>
2024-05-01 20:50:53 +0530

    feat: add drizzle setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as EOS_SCHEMA from 'drizzle/eos/schema/schema';
+import * as LAKEMASTER_SCHEMA from 'drizzle/lakemaster/schema/schema';
+import { DrizzlePostgresModule } from '@knaadh/nestjs-drizzle-postgres';
 
 @Module({
-  imports: [],
+  imports: [
+    DrizzlePostgresModule.registerAsync({
+      tag: 'EOS',
+      useFactory() {
+        return {
+          postgres: {
+            url: process.env.EOS_DATABASE_URL,
+          },
+          config: {
+            schema: { ...EOS_SCHEMA },
+          },
+        };
+      },
+    }),
+    DrizzlePostgresModule.registerAsync({
+      tag: 'LAKEMASTER',
+      useFactory() {
+        return {
+          postgres: {
+            url: process.env.LAKEMASTER_DATABASE_URL,
+          },
+          config: {
+            schema: { ...LAKEMASTER_SCHEMA },
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
